e1b17ba55248c48fb0406e7e0217a5cca3f4cd8a
Victor <126830080+devictor1@users.noreply.github.com>
2024-02-22 19:14:44 -0300

    Duas aulas em um commit
    Não tinha feito na outra aula, fazendo agora no começo da terceira para já atualizar hoje mesmo.

---
+++ victor fg/Flamingo2024Semestre1/src/primeiroprojeto/PrimeiroExemplo.java
@@ -0,0 +1,8 @@
+package primeiroprojeto;
+
+public class PrimeiroExemplo {
+public static void main(String[] args) {
+	System.out.println("Sou o Victor");
+	System.out.print("Sou Auxiliar Financeiro e tenho 23 anos");
+}
+}
